1aad262a94491f3be7362012a10de21b9013ed4e
AravindhRv <aravindhrv4@gmail.com>
2022-05-26 19:30:34 +0530

    Bikes

---
+++ src/test/java/org/gitu/Gittt.java
@@ -0,0 +1,13 @@
+package org.gitu;
+
+public class Gittt {
+private void kawasaki() {
+System.out.println("ninja h2r");
+}
+private void bmw() {
+System.out.println("s1000r");
+}
+private void ducatti() {
+System.out.println("panigle");
+}
+}
